31cec8fedd30c736e30eb77765c80b913038abe1
jimmypc92 <jimmypcampbell92@yahoo.com>
2016-07-18 23:40:01 -0700

    Initial commit.

---
+++ StarStream/HlsConsumer.cs
@@ -0,0 +1,105 @@
+﻿namespace StarStream
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading;
+
+    class HlsConsumer
+    {
+        private Queue<string> _queue;
+        private string _m3u8Uri;
+        private string _rootDir;
+        private Thread _m3u8Thread;
+        private Thread _tsThread;
+        private bool _shouldStop;
+        private M3U8 _m3u8;
+
+        public Action<byte[]> ReceivedSegment { get; set; }
+        public Action<string[]> RecievedExtinf { get; set; }
+
+        public HlsConsumer(string m3u8Uri)
+        {
+            this._queue = new Queue<string>();
+            this._m3u8Uri = m3u8Uri;
+        }
+
+        public void Consume()
+        {
+            if(this._tsThread != null || this._m3u8Thread != null) {
+                throw new InvalidOperationException("Already started consuming.");
+            }
+
+            this._tsThread = new Thread(this._ConsumeTs);
+            this._tsThread.Start(null);
+
+            this._m3u8Thread = new Thread(this._ConsumeM3u8);
+            this._m3u8Thread.Start(null);
+        }
+
+        private void _ConsumeTs(object threadStartParam)
+        {
+            string tsUrl = _m3u8Uri.Substring(0, this._m3u8Uri.IndexOf("index-live.m3u8"));
+
+            using (HttpClient client = new HttpClient()) {
+
+                while(!_shouldStop) {
+
+                    if(this._queue.Count < 1) {
+                        continue;
+                    }
+
+                    var indexPath = this._queue.Dequeue();
+                    var url = tsUrl + indexPath;
+
+                    var res = client.GetAsync(url).Result;
+                    var bytes = res.Content.ReadAsByteArrayAsync().Result;
+
+                    if(this.ReceivedSegment != null) {
+                        this.ReceivedSegment(bytes);
+                    }
+                }
+            }
+        }
+
+        private void _ConsumeM3u8(object threadStartParam)
+        {
+            ExtinfIndex extIndex;
+            int currentIndex = -1;
+
+            using (HttpClient client = new HttpClient()) {
+
+                while (!_shouldStop) {
+                    string content = client.GetAsync(this._m3u8Uri).Result.Content.ReadAsStringAsync().Result;
+
+                    var lines = content.Split('\n');
+
+                    for (int i = 0; i < lines.Length; i++) {
+                        if (ExtinfIndex.IsExtinfIndex(lines[i])) {
+                            extIndex = new ExtinfIndex(lines[i]);
+
+                            int index = extIndex.Index;
+
+                            if (index > currentIndex) {
+                                currentIndex = index;
+                                this._queue.Enqueue(lines[i]);
+                                string[] extinf = new string[2];
+                                extinf[0] = lines[i - 1];
+                                extinf[1] = lines[i];
+
+                                if(this.RecievedExtinf != null) {
+                                    this.RecievedExtinf(extinf);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            this._shouldStop = true;
+        }
+    }
+}
---
+++ StarStream/MP3U8.cs
@@ -0,0 +1,98 @@
+﻿namespace StarStream
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class M3U8
+    {
+        public const string FILE_MARKER = "#EXTM3U";
+        public const string VERSION_MARKER = "#EXT-X-VERSION:";
+        public const string TARGET_DURATION_MARKER = "#EXT-X-TARGETDURATION:";
+        public const string MEDIA_SEQUENCE_MARKER = "#EXT-X-MEDIA-SEQUENCE:";
+        public const string EXTINF_MARKER = "#EXTINF:";
+        public const string END_MARKER = "#EXT-X-ENDLIST";
+
+
+        public int Version { get; set; }
+        public int MediaSequence { get; set; }
+        public int TargetDuration { get; set; }
+        public List<string[]> Extinfs { get; set; }
+
+
+        public M3U8(string content)
+        {
+            this.Extinfs = new List<string[]>();
+
+            string[] lines = content.Split('\n');
+
+            if(lines.Length == 0 || lines[0] != FILE_MARKER) {
+                throw new Exception("Invalid M3U8 file. Missing file identifier.");
+            }
+
+            for(int i = 0; i < lines.Length; i++) {
+
+                switch(lines[i]) {
+                    case FILE_MARKER:
+                        break;
+                    case VERSION_MARKER:
+                        this.Version = int.Parse(lines[i].Substring(VERSION_MARKER.Length, lines.Length - VERSION_MARKER.Length));
+                        break;
+                    case TARGET_DURATION_MARKER:
+                        this.TargetDuration = int.Parse(lines[i].Substring(TARGET_DURATION_MARKER.Length, lines.Length - TARGET_DURATION_MARKER.Length));
+                        break;
+                    case MEDIA_SEQUENCE_MARKER:
+                        this.MediaSequence = int.Parse(lines[i].Substring(MEDIA_SEQUENCE_MARKER.Length, lines.Length - MEDIA_SEQUENCE_MARKER.Length));
+                        break;
+                    case EXTINF_MARKER:
+                        string[] pair = new string[2];
+                        pair[0] = lines[i];
+                        pair[1] = lines[i + 1];
+                        this.Extinfs.Add(pair);
+                        break;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string cr = "\r";
+            sb.Append(FILE_MARKER);
+            sb.Append(cr);
+            sb.Append($"{VERSION_MARKER}{this.Version}");
+            sb.Append(cr);
+            sb.Append($"{MEDIA_SEQUENCE_MARKER}{this.MediaSequence}");
+            sb.Append(cr);
+            sb.Append($"{TARGET_DURATION_MARKER}{this.TargetDuration}");
+            sb.Append(cr);
+
+            foreach(var pair in this.Extinfs) {
+                sb.Append(pair[0]);
+                sb.Append(cr);
+                sb.Append(pair[1]);
+                sb.Append(cr);
+            }
+
+            sb.Append(END_MARKER);
+            sb.Append(cr);
+
+            return sb.ToString();
+        }
+    }
+
+    public class ExtinfIndex
+    {
+        public int Index { get; set; }
+
+        public ExtinfIndex(string line)
+        {
+            this.Index = int.Parse(line.Substring(6, 10));
+        }
+
+        public static bool IsExtinfIndex(string line)
+        {
+            return line.StartsWith("index", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
